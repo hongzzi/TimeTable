0f0750333cc2bb83469dba004930a7e8a2af9d1f
hongzzi <hongzzi.dev@gmail.com>
2019-11-01 16:56:35 +0900

    modify app.js / function list

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,33 +3,36 @@
  */
 
 function list() {
+	
 	$.ajax({
 		url:"http://localhost:5000/timetable/rest/courses",
 		type:"get",
 		success:function(resTxt){
+			console.log("ajax");
 			if(resTxt.status) {
 				let data = resTxt.data;
 				$(".list-lecture").empty();
 				$.each(data, function(idx, item) {
-					let dayofweek = item.dayofweek.split("");
+					let dayofweek = item.dayofweek;
 					let dayStr = "";
 					dayofweek.split("").forEach((day)=>{
-						res+="("+day
-					})
-					let uri = "http://localhost:5000/timetable/rest/courses/detail/";
+						dayStr+="("+day+") ";
+					});
+					let uri = "http://localhost:5000/timetable/rest/courses/detail/"+item.code;
 					let $li = $("<li class='card-lecture'>");
-					$li.append("<li class='card-lecture'> <a class='lecture-title' href="+uri+item.code+">"+item.lecture+"</a>");
+					$li.append("<li class='card-lecture'> <a class='lecture-title' href="+uri+">"+item.lecture+"</a>");
 					$li.append("<h6 class='lecture-time'> <i class='material-icons ic-lecture-info'>access_time</i>");
-					$li.append("<span>"+item.startTime+":00 - "+item.endTime+":50 | "+(월), (수)+"</span></h6>");
+					$li.append("<span>"+item.startTime+":00 - "+item.endTime+":50 | "+dayStr+"</span></h6>");
 					$li.append("<ul class='list-lecture-info'>");
 					$li.append("<li>교과목 코드 : "+item.code+"</li>");
 					$li.append("<li>담당 교수 : "+item.professor+"</li>");
 					$li.append("<li>강의실 : "+item.location+"</li></ul></li>");
+					$(".list-lecture").append($li)
 				})
 			}
 		},
 		error:function(error){
-			
+			alert("오류");
 		}
 	})
 };
